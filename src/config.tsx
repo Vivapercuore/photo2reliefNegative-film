2aff87e980c024f13b252953a2372510cdcaf6bf
陈国锋 <chenguofeng@bytedance.com>
2024-06-26 18:06:39 +0800

    feat describe

--- src/config.tsx
+++ src/config.tsx
@@ -280,7 +280,7 @@ function Config() {
           <div className="title">精细度(整数)</div>
           <div className="describe">
             每mm有多少个像素,和打印机的xy分辨率有关
-          </div>
+          </div> 
           <div className="describe">
             建议A1/P1/X1 设置分别为: 4/8/10
             ,不建议过高,和打印机的,质量-精度-分辨率,有关
